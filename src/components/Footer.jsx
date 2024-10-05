5e8de93ce53fd6a272dfb42df5efc4188f0f2f81
rifki yudha <rifkiydh@gmail.com>
2024-10-05 23:50:37 +0700

    add create a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,19 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="contaner flex justify-between  p-3 bg-sky-400 px-5">
+        <p>
+          <i class="fa-solid fa-copyright"></i>copyright By <span className="font-bold">Ngoding</span>
+        </p>
+        <div className="mediasocial items-center flex gap-3">
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-square-whatsapp"></i>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-square-instagram"></i>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
